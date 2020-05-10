4755320d36cef522cf8b3ba5cd534d4a0c4fe195
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-05-10 11:41:38 -0600

    installed vector-icon library. built first tweet

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import TweetView from './component/view'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TweetView />
     </View>
   );
 }
--- component/view.tsx
+++ component/view.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View >
+        <Text>Ahmed Yagoub</Text>
+      </View>
+      <View >
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+      <View style={styles.footer}>
+        <Icon name='heart' />
+        <Icon name='comment' />
+        <Icon name='share' />
+        <Icon name='retweet' />
+      </View>
     </View>
   );
 }
@@ -13,7 +25,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
+  }
 });
